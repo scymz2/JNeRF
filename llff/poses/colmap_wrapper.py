cde286414a98806c01cebc7a75811583a4fdd23e
scymz2 <mochuan.zhan@manchester.ac.uk>
2024-06-03 21:52:15 +0100

    Enable GPU usage for SiftExtraction in colmap_wrapper.py

--- llff/poses/colmap_wrapper.py
+++ llff/poses/colmap_wrapper.py
@@ -32,7 +32,7 @@ def run_colmap(basedir, is_stereo, match_type):
                 '--database_path', os.path.join(basedir, 'database.db'),
                 '--image_path', os.path.join(basedir, 'images'),
                 '--ImageReader.single_camera_per_folder', '1',
-                '--SiftExtraction.use_gpu', '0',
+                '--SiftExtraction.use_gpu', '1',
         ]
     else:
         feature_extractor_args = [
@@ -40,7 +40,7 @@ def run_colmap(basedir, is_stereo, match_type):
                 '--database_path', os.path.join(basedir, 'database.db'), 
                 '--image_path', os.path.join(basedir, 'images'),
                 '--ImageReader.single_camera', '1',
-                '--SiftExtraction.use_gpu', '0',
+                '--SiftExtraction.use_gpu', '1',
         ]
 
     feat_output = ( subprocess.check_output(feature_extractor_args, universal_newlines=True) )
